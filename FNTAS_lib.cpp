ba7ae97dd83787888ef019ff15ab24fb8e766f9e
FerrumNitka <nikita14abc@gmail.com>
2025-09-02 14:59:20 +0700

    Add files via upload

---
+++ FNTAS_lib.cpp
@@ -0,0 +1,46 @@
+﻿#include "FNTAS_lib.h"
+
+
+void FNTAS_lib::play_wav_file(LPCWSTR file_name)
+{
+	PlaySound(file_name, NULL, SND_FILENAME | SND_ASYNC);
+}
+
+void FNTAS_lib::window_transparency(bool on_off)
+{
+	if (on_off == true)
+	{
+		SetLayeredWindowAttributes(GetConsoleWindow(), RGB(0, 0, 0), 255, LWA_ALPHA);
+	}
+	else
+	{
+		SetLayeredWindowAttributes(GetConsoleWindow(), RGB(0, 0, 0), 255-45, LWA_ALPHA);
+	}
+}
+
+void FNTAS_lib::window_title(LPCWSTR title)
+{
+	SetConsoleTitle(title);
+}
+
+void FNTAS_lib::wait_seconds(unsigned char seconds)
+{
+	Sleep(1000 * seconds);
+}
+
+void FNTAS_lib::clear()
+{
+	// Костыль
+	COORD coord;
+	coord.X = 0;
+	coord.Y = 0;
+
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+
+	for (unsigned char i = 0; i < 15; i++)
+	{
+		std::cout << "                      " << std::endl;
+	}
+
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
---
+++ FNTAS_lib.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include <iostream>
+#include <string>
+#include <Windows.h>
+#include <mmsystem.h>
+#pragma comment(lib, "Winmm.lib")
+
+
+namespace FNTAS_lib
+{
+	// void print_title();
+	// void repeat_char_output();
+	void play_wav_file(LPCWSTR file_name);	// Воспроизводит аудиофайлы формата wav
+	void window_transparency(bool on_off); // включает и выключает прозрачность окна
+	void window_title(LPCWSTR title); // Задаёт заголовок окна
+	void wait_seconds(unsigned char seconds); // Задержка
+	void clear(); // Отчищает терминал от символов
+}
